f2380a95df632574c12226b5c9b6d625abaf859b
AnttiTup <anttitup@cs.helsinki.fi>
2010-06-15 15:22:07 +0300

    more specs

--- spec/controllers/logger_controller_spec.rb
+++ spec/controllers/logger_controller_spec.rb
@@ -1,16 +1,32 @@
-require 'spec_helper'
 
 describe LoggerController do
 
-  #Delete this example and add some real ones
-  it "should use LoggerController" do
-    controller.should be_an_instance_of(LoggerController)
+  before(:each) do
+  
   end
+ 
   it "should create sc_consumer consumer" do
-    sc.consumer.should_not be_a nil
+    logger = logger_controller.new()
+    sc_consumer.should_not be_a nil
   end
 
-  it "should redirect in" do
-    
+  it "should create a new session" do
+    session[:sc_request_token].should_not be_a nil
+    session[sc_request_token_secret].should_not be_a nil
+  end
+  
+  it "should redirect on right site" do
+    @authorize_url.should include "http://soundcloud.com/oauth/authorize"
+    redirect?.should be_success
+  end
+  
+  it "should next login and create new  user" do
+    user.should be_an_instance_of(User)
+    session[:user_id].should == user.id
+    redirect?.should be_success
+  end
+  it "should empty session and redirect on home" do
+    session[:user_id].should be_a nil
+    redirect.should be_success
   end
 end
